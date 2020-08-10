43f0d6295001e5424cd53f3f71742fe1bb71e401
magicalcyber <magicalcyber@10.108.138.142>
2020-08-10 18:32:54 +0700

    Init Commit

---
+++ module1/src/main/java/cc/magickiat/demo/maven/module1/MainModule1.java
@@ -0,0 +1,9 @@
+package cc.magickiat.demo.maven.module1;
+
+public class MainModule1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Module 1");
+	}
+
+}
---
+++ module2/src/main/java/cc/magickiat/demo/maven/module2/MainModule2.java
@@ -0,0 +1,9 @@
+package cc.magickiat.demo.maven.module2;
+
+public class MainModule2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Module 2");
+	}
+
+}
